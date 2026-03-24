98f831bbeac7f5408cba1503a66171ea72ca78bb
DevruneHaz <damonhazelden0@gmail.com>
2026-03-24 13:09:35 +0000

    Generalised Rendering
    Created a rendering scene that adds the camera and window to any object. The size of the window is configurable.

--- scripts/frog.gd
+++ scripts/frog.gd
@@ -3,13 +3,8 @@ extends CharacterBody2D
 
 @onready var game_manager: Node = %GameManager
 @onready var frog: CharacterBody2D = $"."
-@onready var _MainWindow: Window = game_manager._MainWindow
-@onready var window: Window = $Window
+@onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
 
-var hovering = false
-
-func _ready() -> void:
-	window.world_2d = _MainWindow.world_2d
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -17,12 +12,3 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	move_and_slide()
-
-
-func _on_mouse_entered() -> void:
-	hovering = true
-	game_manager.hovering = frog
-
-func _on_mouse_exited() -> void:
-	hovering = false
-	game_manager.hovering = null
--- scripts/window.gd
+++ scripts/window.gd
@@ -1,11 +1,14 @@
 extends Window
 
 @onready var _Camera: Camera2D = $Camera2D
-@onready var frog: CharacterBody2D = $".."
+@onready var focus: Node2D = $".."
+@export var focusSize: Vector2
 @onready var window: Window = $"."
+@onready var game_manager: Node = GameManager
 
 var last_position: = Vector2i.ZERO
 var velocity: = Vector2i.ZERO
+var hovering = false
 
 func _ready() -> void:
 	# Set the anchor mode to "Fixed top-left"
@@ -13,13 +16,26 @@ func _ready() -> void:
 	_Camera.anchor_mode = Camera2D.ANCHOR_MODE_FIXED_TOP_LEFT
 	
 	close_requested.connect(queue_free) # Actually close the window when clicking the close button
+	
+	window.world_2d = game_manager._MainWindow.world_2d # Sets the window to view the main world
+	
+	window.size = focusSize * 6
+	
 
 func _process(_delta: float) -> void:
 	velocity = position - last_position
 	last_position = position
 	_Camera.position = get_camera_pos_from_window()
-	window.position = Vector2(frog.position.x - 96, frog.position.y - 96)
+	window.position = Vector2(focus.position.x - 96, focus.position.y - 96)
 
 func get_camera_pos_from_window()->Vector2i:
 	return position + velocity
-	
+
+func _on_mouse_entered() -> void:
+	hovering = true
+	game_manager.hovering = focus
+	print("hovering")
+
+func _on_mouse_exited() -> void:
+	hovering = false
+	game_manager.hovering = null
