5d34b2cbf4a8e7acf5d210328d3633ec2fa2a100
DevruneHaz <damonhazelden0@gmail.com>
2026-04-17 15:13:54 +0100

    Begin adding grabbing to ball
    Adding the grabbing functionality that the frog has to the ball.

---
+++ Godot Project/scenes/ball.gd
@@ -0,0 +1,29 @@
+extends RigidBody2D
+
+@onready var game_manager: Node = %GameManager
+var speed: float
+var maxSpeed: int = 355000
+var direction: Vector2
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _physics_process(delta: float) -> void:
+#	#Make pet attach to cursor
+#	direction = (get_global_mouse_position() - position).normalized()
+#	game_manager.hovering = self
+#	velocity = Vector2(0, 0)
+#		
+#	if speed >= maxSpeed:
+#		speed = maxSpeed
+#	else:
+#		speed = position.distance_to(get_global_mouse_position()) * speedMultiplier
+#	
+#	var collision = move_and_collide(direction * speed * delta)
+#	if collision:
+#		direction = direction.bounce(collision.get_normal())
+#	
+#	move_and_slide()
